f92699988a219afeb0c7efcd3a7eab98499b1b33
Uttam Maurya <uttammaurya7404@gmail.com>
2025-03-13 02:30:59 +0530

    code uncommented

--- pattern.py
+++ pattern.py
@@ -734,11 +734,11 @@ for i in range(1, n):
 # D D D
 # O O O O
 # C C C C C
-# s="CODER"
-# n=len(s)
-# p=n-1
-# for i in range (n):
-#     for j in range (i+1):
-#         print(s[p], end=" ")
-#     p-=1
-#     print() 
+s="CODER"
+n=len(s)
+p=n-1
+for i in range (n):
+    for j in range (i+1):
+        print(s[p], end=" ")
+    p-=1
+    print() 
